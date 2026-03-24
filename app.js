95f49c804eb4233d4bee0034d9917b6e3520f26d
mariagomez08 <mariiangelgomezsg@gmail.com>
2026-03-24 15:09:59 -0500

    Actualizar contador inicial a 0 y ajustar colores en función del valor del contador

--- app.js
+++ app.js
@@ -2,34 +2,28 @@ const numeroContador = document.getElementById("numeroContador");
 const botonAumentar = document.getElementById("btnAumentar");
 const botonDecrementar = document.getElementById("btnDecrementar");
 
-let aux = 1;
+let aux = 0;
 
-let fuePulsado = false;
-
-console.log("Estoy aqui");
-
-botonAumentar.addEventListener("click", () => {
-  fuePulsado = true;
-  if (fuePulsado && aux <10) {
+botonAumentar.addEventListener("click", function () {
+  if (aux < 9) {
     aux = aux + 1;
     numeroContador.textContent = aux; // <--- aqui pasamos el dato al dom, obtener o modificar el contenido de texto
-    document.numeroContador = aux;
-  }else{
-    numeroContador.style.color = "blue"
+    numeroContador.style.color = "white";
+  } else if (aux === 9) {
+    aux = aux + 1;
+    numeroContador.textContent = aux;
+    numeroContador.style.color = "red";
   }
 });
 
-botonDecrementar.addEventListener("click", () => {
-  fuePulsado = true;
-  if (fuePulsado && aux > 0) {
+botonDecrementar.addEventListener("click", function () {
+  if (aux > 1) {
     aux = aux - 1;
-    numeroContador.textContent = aux; // <--- aqui pasamos el dato al dom, obtener o modificar el contenido de texto, 
-    document.numeroContador = aux;
-    if(aux === 1){
-        return
-    }
-  }else{
-    numeroContador.style.color = "red"
+    numeroContador.textContent = aux; // <--- aqui pasamos el dato al dom, obtener o modificar el contenido de texto,
+    numeroContador.style.color = "white";
+  } else if (aux === 1) {
+    aux = aux - 1;
+    numeroContador.textContent = aux;
+    numeroContador.style.color = "red";
   }
 });
-
