866941ba286963dc397261dfa5306390a6ebb9ef
mariagomez08 <mariiangelgomezsg@gmail.com>
2026-03-24 12:48:08 -0500

    Se agregan avances sobre el boton decrementar

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const numeroContador = document.getElementById("numeroContador");
 const botonAumentar = document.getElementById("btnAumentar");
+const botonDecrementar = document.getElementById("btnDecrementar");
 
 let aux = 1;
+let aux2 = 10;
+
 let fuePulsado = false;
 
 console.log("Estoy aqui");
@@ -14,6 +17,17 @@ botonAumentar.addEventListener("click", () => {
     document.numeroContador = aux;
   }else{
     numeroContador.style.color = "blue"
+  }
+});
+
+botonDecrementar.addEventListener("click", () => {
+  fuePulsado = true;
+  if (fuePulsado && aux2 > 0) {
+    aux = aux - 1;
+    numeroContador.textContent = aux; // <--- aqui pasamos el dato al dom
+    document.numeroContador = aux;
+  }else{
+    numeroContador.style.color = "red"
   }
 });
 
