f5899b989d23c7031ea8c26d11fe81678ab1fd7d
mariagomez08 <mariiangelgomezsg@gmail.com>
2026-03-24 12:38:07 -0500

    Se agregan avances del contador en app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const numeroContador = document.getElementById("numeroContador");
 const botonAumentar = document.getElementById("btnAumentar");
 
+
+
 let aux = 1;
 let fuePulsado = false;
 
@@ -8,9 +10,11 @@ console.log("Estoy aqui");
 
 botonAumentar.addEventListener("click", () => {
   fuePulsado = true;
-  if (fuePulsado) {
+  if (fuePulsado && aux <10) {
     aux = aux + 1;
     numeroContador.textContent = aux; // <--- AQUÍ SE PASA EL DATO
     document.numeroContador = aux;
+  }else{
+    numeroContador.style.color = "blue"
   }
 });
